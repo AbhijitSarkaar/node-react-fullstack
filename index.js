a6507342b3ccc1a04d5ac2e972ae291be1139695
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-10-24 05:30:20 +0530

    google redirection to redirect url after authentication

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+dotenv.config()
+
+const {CLIENT_ID, CLIENT_SECRET} = process.env
 
 const app = express()
 
+passport.use(new GoogleStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile) => {
+    console.log('accessToken', accessToken)
+    console.log('refreshToken', refreshToken)
+    console.log('profile', profile)
+}))
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 
