eeb2276a64c9022a976009e064b8e3cbeb86fb4f
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2023-10-24 07:58:22 +0530

    server architecture changes

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
 const express = require('express')
-const dotenv = require('dotenv')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-dotenv.config()
-
-const {CLIENT_ID, CLIENT_SECRET} = process.env
+require('./services/passport')
 
 const app = express()
-
-passport.use(new GoogleStrategy({
-    clientID: CLIENT_ID,
-    clientSecret: CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile) => {
-    console.log('accessToken', accessToken)
-    console.log('refreshToken', refreshToken)
-    console.log('profile', profile)
-}))
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 
