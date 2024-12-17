a06c06f4dd4be3958d62098bd2978ca1d07bbf56
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2024-12-17 18:07:57 +0530

    session based authentication flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,38 @@ const passport = require("passport");
 
 module.exports = (app) => {
   app.get(
+    //initial request uri
     "/auth/google",
+
+    //passport initialises authentication by calling google oauth system
     passport.authenticate("google", {
       scope: ["profile", "email"],
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    // callback url once google oauth flow returns a code
+    "/auth/google/callback",
+
+    // middleware
+    // passport sends 'code' to google to get users profile data
+    // once authenticated passport calls serializeUser to set user data in req.session
+    passport.authenticate("google"),
+
+    // route handler once user has been authenticated
+    (req, res) => {
+      res.send(req.session);
+    }
+  );
+
+  app.get("/api/current_user", (req, res) => {
+    // res.send(req.session);
+    res.send(req.user);
+  });
+
+  app.get("/api/logout", (req, res) => {
+    //added to request by passport
+    req.logout();
+    res.send("logged out");
+  });
 };
--- services/passport.js
+++ services/passport.js
@@ -1,11 +1,26 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 //callback redirect uri is specific to client application providing google sign in to users
 //it is to securely provide the application with a code through the redirect uri
 //redirect uri is configured in the google account of the application and it is secret
 
+//purpose of oauth flow is to receive a unique id of a user
+// from a third party providing oauth authentication
 passport.use(
   new GoogleStrategy(
     {
@@ -14,9 +29,23 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
+      //check if user already exists
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //user already exists
+          done(null, existingUser);
+        } else {
+          //new mongoose model instance created
+          new User({
+            googleId: profile.id,
+          })
+            // mongoose model instance saved
+            .save()
+            .then((user) => {
+              done(null, user);
+            });
+        }
+      });
     }
   )
 );
